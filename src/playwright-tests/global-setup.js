91a8974072d9e282fa37d93caf9188b529ca6d9d
Olakunle Neye <olakunle.neye@thoughtworks.com>
2023-05-03 20:20:11 -0500

    adds some extra info for reference

--- src/playwright-tests/global-setup.js
+++ src/playwright-tests/global-setup.js
@@ -1,12 +1,13 @@
 import { chromium } from "playwright";
 
-// Reference: https://playwright.dev/docs/test-global-setup-teardown
 /**
- * 
+ * Reference: https://playwright.dev/docs/test-global-setup-teardown
  * @param {import("@playwright/test").FullConfig} config 
  */
 async function globalSetup(config ) {
+  // const { baseURL, storageState } = config.projects[0].use;
   const browser = await chromium.launch();
+  // perform global setup like authentication and so on here
   await browser.close();
 }
 
